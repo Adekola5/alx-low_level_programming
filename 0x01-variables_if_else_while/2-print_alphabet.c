0a6a9dcb6c52714438c2f2387a30e62b15cd15a2
Adekola5 <aadekola93@gmail.com>
2022-12-09 13:35:01 -0800

    C is hard

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main - Entry point
+ * main - main block
+ * Description: Get a random it with 5
  * Return: 0
  */
 int main(void)
 {
 
+	char c = 'a';
 
-	char c;
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
 
-       c= 'a';
-       while
-	       	(c <= 'z') {
-		        putchar(c);
-		        c++;
-	        }
 	putchar('\n');
 	return (0);
 }
