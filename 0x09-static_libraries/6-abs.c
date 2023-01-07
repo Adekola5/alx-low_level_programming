09b38bd3eafa0bd7f98f1ccd135cfb931713166e
Adekola5 <aadekola93@gmail.com>
2023-01-07 06:22:04 -0800

    qwert

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value for an interger
+ * @n: interger to be tested
+ * Return: 0 or 1
+ */
+int _abs(int n)
+{
+	return (n < 0 ? -1 * n : n);
+}
