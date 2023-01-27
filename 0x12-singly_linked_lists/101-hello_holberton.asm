d3f9d3291b9ad4cb568a9daf21299d6be5058bb5
Adekola5 <aadekola93@gmail.com>
2023-01-27 14:54:05 -0800

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,12 +1,9 @@
 global    main
-	extern    printf
-
-	section .text
+extern    printf
 main:
 	mov   edi, format
 	xor   eax, eax
 	call  printf
 	mov 	eax, 0
 	ret
-message:   db "Hello, Holberton", 0
-format:    db "%s", 10, 0
+format: db `Hello, Holberton\n`,0
